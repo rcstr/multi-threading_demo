4a5867a12aa5aa06921d85e02d7a903776fcbfa4
Rommel Castro <rommelxcastro@gmail.com>
2022-08-13 12:27:59 -0600

    process.js

---
+++ process.js
@@ -0,0 +1,10 @@
+// > node process.js A &
+const process_name = process.argv.slice(2)[0];
+
+count = 0;
+while (true) {
+	count++;
+	if (count === 2000 || count === 4000) {
+		console.log(`${process_name}: ${count}`);
+	}
+}
